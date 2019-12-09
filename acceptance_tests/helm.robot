5d1b9fc4ce8188d9de0246072c934fdc694d8510
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2019-12-08 23:21:25 -0600

    run install test on both helm 2 & 3
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>

--- acceptance_tests/helm.robot
+++ acceptance_tests/helm.robot
@@ -6,6 +6,22 @@ Suite Setup       Suite Setup
 Suite Teardown    Suite Teardown
 
 *** Test Cases ***
+Plugin installs on Helm 2
+    Test plugin installation   2
+
+Plugin installs on Helm 3
+    Test plugin installation   3
+
+*** Keywords ***
+Test plugin installation
+    [Arguments]    ${version}
+    set helm version    ${version}
+    helm-push can be installed as a Helm plugin
+    helm-push is listed as a Helm plugin after install
+    helm-push can be run as a Helm plugin
+    helm-push can be removed
+    helm-push is not listed as a Helm plugin after removal
+
 helm-push can be installed as a Helm plugin
     install helm plugin
     return code should be   0
@@ -26,9 +42,14 @@ helm-push is not listed as a Helm plugin after removal
     check helm plugin
     return code should not be   0
 
-*** Keywords ***
 Suite Setup
+    set helm version    3
+    remove helm plugin
+    set helm version    2
     remove helm plugin
 
 Suite Teardown
+    set helm version    3
+    remove helm plugin
+    set helm version    2
     remove helm plugin
--- acceptance_tests/lib/Helm.py
+++ acceptance_tests/lib/Helm.py
@@ -1,22 +1,33 @@
 import common
 
+HELM_EXE = 'helm2'
 
 class Helm(common.CommandRunner):
+    def set_helm_version(self, version):
+        global HELM_EXE
+        version = str(version)
+        if version == '2':
+            HELM_EXE = 'helm2'
+        elif version == '3':
+            HELM_EXE = 'helm3'
+        else:
+            raise Exception('invalid Helm version provided: %s' % version)
+
     def add_chart_repo(self):
         self.remove_chart_repo()
-        self.run_command('helm2 repo add %s %s' % (common.HELM_REPO_NAME, common.HELM_REPO_URL))
+        self.run_command('%s repo add %s %s' % (HELM_EXE, common.HELM_REPO_NAME, common.HELM_REPO_URL))
 
     def remove_chart_repo(self):
-        self.run_command('helm2 repo remove %s' % common.HELM_REPO_NAME)
+        self.run_command('%s repo remove %s' % (HELM_EXE, common.HELM_REPO_NAME))
 
     def install_helm_plugin(self):
-        self.run_command('helm2 plugin install %s' % self.rootdir)
+        self.run_command('%s plugin install %s' % (HELM_EXE, self.rootdir))
 
     def check_helm_plugin(self):
-        self.run_command('helm2 plugin list | grep ^push')
+        self.run_command('%s plugin list | grep ^push' % HELM_EXE)
 
     def run_helm_plugin(self):
-        self.run_command('helm2 push --help')
+        self.run_command('%s push --help' % HELM_EXE)
 
     def remove_helm_plugin(self):
-        self.run_command('helm2 plugin remove push')
+        self.run_command('%s plugin remove push' % HELM_EXE)
