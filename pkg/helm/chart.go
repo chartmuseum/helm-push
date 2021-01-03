039407a0b5cc0a89069f0c8663520a927443b75f
Karuppiah Natarajan <karuppiah7890@gmail.com>
2021-01-03 12:38:10 +0530

    use helm v3 library to load and package both helm v2 and helm v3 charts
    this reverts the golang code changes made in 17794c3f46c90a7518814cdac5bb834c3adc9708

--- pkg/helm/chart.go
+++ pkg/helm/chart.go
@@ -4,56 +4,31 @@ import (
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/chartutil"
-	v2chartutil "k8s.io/helm/pkg/chartutil"
-	v2chart "k8s.io/helm/pkg/proto/hapi/chart"
 )
 
 type (
 	// Chart is a helm package that contains metadata
 	Chart struct {
-		V3 *chart.Chart
-		V2 *v2chart.Chart
+		*chart.Chart
 	}
 )
 
 // SetVersion overrides the chart version
 func (c *Chart) SetVersion(version string) {
-	if c.V2 != nil {
-		c.V2.Metadata.Version = version
-	} else {
-		c.V3.Metadata.Version = version
-	}
+	c.Metadata.Version = version
 }
 
 // GetChartByName returns a chart by "name", which can be
 // either a directory or .tgz package
 func GetChartByName(name string) (*Chart, error) {
-	c := &Chart{}
-	v3c, err := loader.Load(name)
+	cc, err := loader.Load(name)
 	if err != nil {
 		return nil, err
 	}
-
-	// If the Helm 2 API version (v1) is detected, use the old
-	// method to load the chart
-	if v3c.Metadata.APIVersion == chart.APIVersionV1 {
-		v2c, err := v2chartutil.Load(name)
-		if err != nil {
-			return nil, err
-		}
-		c.V2 = v2c
-	} else {
-		c.V3 = v3c
-	}
-
-	return c, nil
+	return &Chart{cc}, nil
 }
 
 // CreateChartPackage creates a new .tgz package in directory
 func CreateChartPackage(c *Chart, outDir string) (string, error) {
-	if c.V2 != nil {
-		return v2chartutil.Save(c.V2, outDir)
-	} else {
-		return chartutil.Save(c.V3, outDir)
-	}
+	return chartutil.Save(c.Chart, outDir)
 }
